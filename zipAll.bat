370296f695bfce937d3ed9a51353416446e88a22
Mohamed Shalaby <41000506+ctybrd@users.noreply.github.com>
2026-01-05 16:03:22 +0200

    qeraat

--- zipAll.bat
+++ zipAll.bat
@@ -12,12 +12,15 @@ winrar a -afzip -ep1 -o+ -idq "data_v19.db.zip" "data_v19.db"
 winrar a -afzip -ep1 -o+ -idq "data_v20.db.zip" "data_v20.db"
 winrar a -afzip -ep1 -o+ -idq "data_v20.zip" "data_v20.db"
 
+winrar a -afzip -ep1 -o+ -idq "data_v21.db.zip" "data_v21.db"
+winrar a -afzip -ep1 -o+ -idq "data_v22.db.zip" "data_v22.db"
 :: farsh_v10
 winrar a -afzip -ep1 -o+ -idq "farsh_v10.db.zip" "farsh_v10.db"
 
 :: farsh_v11
 winrar a -afzip -ep1 -o+ -idq "farsh_v11.zip" "farsh_v11.db"
 winrar a -afzip -ep1 -o+ -idq "farsh_v11.db.zip" "farsh_v11.db"
+winrar a -afzip -ep1 -o+ -idq "farsh_v12.db.zip" "farsh_v12.db"
 
 :: words_data_simple
 winrar a -afzip -ep1 -o+ -idq "words_data_simple_v1.zip" "words_data_simple.db"
@@ -25,6 +28,7 @@ winrar a -afzip -ep1 -o+ -idq "words_data_simple_v1.zip" "words_data_simple.db"
 :: words_v1
 winrar a -afzip -ep1 -o+ -idq "words_v1.zip" "words_v1.db"
 winrar a -afzip -ep1 -o+ -idq "words_v1.db.zip" "words_v1.db"
-
+winrar a -afzip -ep1 -o+ -idq "words_v2.db.zip" "words_v2.db"
+winrar a -afzip -ep1 -o+ -idq "words_v3.db.zip" "words_v3.db"
 echo === All zips updated successfully ===
 pause
